b0f62c48a7ee4d2f92ac60f7ca841951955113da
Muhammad <akun.asifurrohman@gmail.com>
2024-03-15 17:16:09 +0700

    add: profile belum jadi

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,9 @@
 let hamburgerMenu = document.getElementById('hamburgerMenu');
 let closeHamburgerMenu = document.getElementById('closeHamburgerMenu');
 let sidebar = document.getElementById('sidebar');
+let avatarButton = document.getElementById('avatarButton');
+let avatarMenu = document.getElementById('avatarMenu');
+let main = document.getElementById('main');
 
 hamburgerMenu.addEventListener('click', () => {
     if(sidebar.classList.contains('-translate-x-full')){
@@ -13,3 +16,26 @@ hamburgerMenu.addEventListener('click', () => {
 closeHamburgerMenu.addEventListener('click', () => {
     sidebar.classList.add('-translate-x-full');
 });
+
+avatarButton.addEventListener('click', (e) => {
+    if(avatarMenu.classList.contains('hidden')){
+        avatarMenu.classList.remove('hidden');
+        avatarMenu.classList.add('block');
+    } else {
+        avatarMenu.classList.remove('block');
+        avatarMenu.classList.add('hidden');
+    }
+});
+
+main.onclick = (e) => {
+    if(!avatarMenu.classList.contains('hidden') && !avatarMenu.contains(e.target)){
+        avatarMenu.classList.remove('block');
+        avatarMenu.classList.add('hidden');
+        console.log('gak iso wleeek!');
+    }
+
+    if(!sidebar.classList.contains('-translate-x-full') && !sidebar.contains(e.target)){
+        sidebar.classList.remove('translate-x-full');
+        sidebar.classList.add('-translate-x-full');
+    }
+}
